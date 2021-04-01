d56688d14d17f74dda6cb3c753c7e10997b4f1ea
GuoLei1990 <gl3336563@163.com>
2021-04-01 16:50:32 +0800

    fix: material texture display error bug.

--- demos/controls-free/index.ts
+++ demos/controls-free/index.ts
@@ -9,7 +9,11 @@ import {
   MeshTopology,
   WebGLEngine,
   Color,
-  PrimitiveMesh
+  PrimitiveMesh,
+  AssetType,
+  Texture2D,
+  AmbientLight,
+  DirectLight
 } from "oasis-engine";
 
 const engine = new WebGLEngine("o3-demo");
@@ -17,42 +21,68 @@ engine.canvas.resizeByClientSize();
 const scene = engine.sceneManager.activeScene;
 const rootNode = scene.createRootEntity("root");
 
-// 在场景中创建相机节点、配置位置和目标方向
-const cameraNode = rootNode.createChild("camera_node");
-cameraNode.transform.setPosition(0, 0, 20);
+engine.resourceManager
+  .load([
+    {
+      url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*_4RHQYi4PfcAAAAAAAAAAAAAARQnAQ",
+      type: AssetType.Texture2D
+    },
+    {
+      url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*FO6lRbeTVi4AAAAAAAAAAAAAARQnAQ",
+      type: AssetType.Texture2D
+    }
+  ])
+  .then((texture) => {
+    // 在场景中创建相机节点、配置位置和目标方向
+    const cameraNode = rootNode.createChild("camera_node");
+    cameraNode.transform.setPosition(0, 0, 50);
 
-const camera = cameraNode.addComponent(Camera);
-camera.farClipPlane = 2000;
+    const camera = cameraNode.addComponent(Camera);
+    camera.farClipPlane = 2000;
 
-const controler = cameraNode.addComponent(FreeControl);
-controler.movementSpeed = 100;
-controler.rotateSpeed = 1;
+    const controler = cameraNode.addComponent(FreeControl);
+    controler.movementSpeed = 100;
+    controler.rotateSpeed = 1;
 
-const cuboid = PrimitiveMesh.createCuboid(engine, 50, 50, 50);
-const material = new BlinnPhongMaterial(engine);
-material.emissiveColor = new Color(0.5, 0.6, 0.6, 1);
+    const lightEntity = rootNode.createChild("LightEntity");
+    // const ambient = lightEntity.addComponent(AmbientLight);
+    const directLight = lightEntity.addComponent(DirectLight);
+    // ambient.color = new Color(0.5, 0.5, 0.5);
+    directLight.color = new Color(0.5, 0.5, 0.5);
 
-const groundGeometry = PrimitiveMesh.createPlane(engine, 2000, 2000, 100, 100);
-groundGeometry.subMesh.topology = MeshTopology.LineStrip;
-const groundMaterial = new BlinnPhongMaterial(engine);
-groundMaterial.emissiveColor = new Color(1, 1, 1, 1);
+    const cuboid = PrimitiveMesh.createCuboid(engine, 10, 10, 10);
 
-// create meshes in scene
-for (let i = 0; i < 100; i++) {
-  let cube = rootNode.createChild("cube");
-  cube.transform.setPosition(Math.random() * 2000 - 1000, Math.random() * 200, Math.random() * 2000 - 1000);
-  const cubeRenderer = cube.addComponent(MeshRenderer);
-  cubeRenderer.mesh = cuboid;
-  cubeRenderer.setMaterial(material);
-}
+    const material = new BlinnPhongMaterial(engine);
+    material.baseTexture = <Texture2D>texture[0];
 
-// Ground
-const ground = rootNode.createChild("ground");
-ground.transform.setPosition(0, -25, 0);
-ground.transform.rotate(-90, 0, 0);
-const groundRender = ground.addComponent(MeshRenderer);
-groundRender.mesh = groundGeometry;
-groundRender.setMaterial(groundMaterial);
+    const material1 = new BlinnPhongMaterial(engine);
+    material1.baseTexture = <Texture2D>texture[1];
 
-// Run engine
-engine.run();
+    // const groundGeometry = PrimitiveMesh.createPlane(engine, 2000, 2000, 100, 100);
+    // groundGeometry.subMesh.topology = MeshTopology.LineStrip;
+    // const groundMaterial = new BlinnPhongMaterial(engine);
+    // groundMaterial.emissiveColor = new Color(1, 1, 1, 1);
+
+    // create meshes in scene
+    let cube = rootNode.createChild("cube");
+    const cubeRenderer = cube.addComponent(MeshRenderer);
+    cubeRenderer.mesh = cuboid;
+    cubeRenderer.setMaterial(material);
+
+    let cube1 = rootNode.createChild("cube1");
+    cube1.transform.setPosition(-15, 0, 0);
+    const cubeRenderer1 = cube1.addComponent(MeshRenderer);
+    cubeRenderer1.mesh = cuboid;
+    cubeRenderer1.setMaterial(material1);
+
+    // // Ground
+    // const ground = rootNode.createChild("ground");
+    // ground.transform.setPosition(0, -25, 0);
+    // ground.transform.rotate(-90, 0, 0);
+    // const groundRender = ground.addComponent(MeshRenderer);
+    // groundRender.mesh = groundGeometry;
+    // groundRender.setMaterial(groundMaterial);
+
+    // Run engine
+    engine.run();
+  });
--- demos/pbr/index.ts
+++ demos/pbr/index.ts
@@ -28,12 +28,12 @@ const glColor2Color = (color) => new Color(color[0] * 255, color[1] * 255, color
 const gui = new dat.GUI();
 gui.domElement.style = "position:absolute;top:0px;left:50vw";
 
-let envLightNode = rootEntity.createChild("env_light");
-let envLight = envLightNode.addComponent(EnvironmentMapLight);
-let envFolder = gui.addFolder("EnvironmentMapLight");
-envFolder.add(envLight, "enabled");
-envFolder.add(envLight, "specularIntensity", 0, 1);
-envFolder.add(envLight, "diffuseIntensity", 0, 1);
+// let envLightNode = rootEntity.createChild("env_light");
+// let envLight = envLightNode.addComponent(EnvironmentMapLight);
+// let envFolder = gui.addFolder("EnvironmentMapLight");
+// envFolder.add(envLight, "enabled");
+// envFolder.add(envLight, "specularIntensity", 0, 1);
+// envFolder.add(envLight, "diffuseIntensity", 0, 1);
 
 let directLightColor = { color: [255, 255, 255] };
 let directLightNode = rootEntity.createChild("dir_light");
@@ -59,37 +59,6 @@ Promise.all([
     .then((gltf) => {
       rootEntity.addChild(gltf.defaultSceneRoot);
       console.log(gltf);
-    }),
-  engine.resourceManager
-    .load<TextureCubeMap>({
-      urls: [
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*Bk5FQKGOir4AAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*_cPhR7JMDjkAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*trqjQp1nOMQAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*_RXwRqwMK3EAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*q4Q6TroyuXcAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*DP5QTbTSAYgAAAAAAAAAAAAAARQnAQ"
-      ],
-      type: AssetType.TextureCube
-    })
-    .then((cubeMap) => {
-      envLight.diffuseTexture = cubeMap;
-    }),
-  engine.resourceManager
-    .load<TextureCubeMap>({
-      urls: [
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*5w6_Rr6ML6IAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*TiT2TbN5cG4AAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*8GF6Q4LZefUAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*D5pdRqUHC3IAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*_FooTIp6pNIAAAAAAAAAAAAAARQnAQ",
-        "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*CYGZR7ogZfoAAAAAAAAAAAAAARQnAQ"
-      ],
-      type: AssetType.TextureCube
-    })
-    .then((cubeMap) => {
-      envLight.specularTexture = cubeMap;
-      rootEntity.addComponent(SkyBox).skyBoxMap = cubeMap;
     })
 ]).then(() => {
   engine.run();
