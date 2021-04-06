c2e2c1c68ad50043850fb5d3ce6726d6ea5f0248
johanzhu <634606335@qq.com>
2021-04-06 11:31:07 +0800

    feat: change to single json file resource

--- demos/spine-animation/index.ts
+++ demos/spine-animation/index.ts
@@ -12,27 +12,21 @@ const rootEntity = scene.createRootEntity();
 // camera
 const cameraEntity = rootEntity.createChild("camera_node");
 const camera = cameraEntity.addComponent(Camera);
-camera.farClipPlane = 2000000;
-camera.nearClipPlane = 0.001;
-cameraEntity.transform.position = new Vector3(0, 0, 12);
+camera.farClipPlane = 1000;
+cameraEntity.transform.position = new Vector3(0, 0, 70);
 
 engine.resourceManager
   .load({
-    // @ts-ignore
-    urls: [
-      "https://gw.alipayobjects.com/os/OasisHub/dfad57bd-b6ae-4c73-9a77-d74d66ab8757/1617361412465.json",
-      "https://gw.alipayobjects.com/os/OasisHub/bb62c664-59ff-437a-88a8-6c8a42c8bbd7/1617361412468.atlas",
-      "https://gw.alipayobjects.com/zos/OasisHub/72300d5b-d2b0-407d-9f9b-1b0e4f80e935/1617361412468.png"
-    ],
-    // @ts-ignore
+    url: "https://sbfkcel.github.io/pixi-spine-debug/assets/spine/spineboy-pro.json",
     type: "spine"
   })
   .then((spineEntity: Entity) => {
+    spineEntity.transform.setPosition(0, -12, 0);
     rootEntity.addChild(spineEntity);
     const spineAnimation = spineEntity.getComponent(SpineAnimation);
-    spineAnimation.state.setAnimation(0, "animation", true);
-    spineAnimation.skeleton.scaleX = 0.007;
-    spineAnimation.skeleton.scaleY = 0.007;
+    spineAnimation.state.setAnimation(0, "walk", true);
+    spineAnimation.skeleton.scaleX = 0.05;
+    spineAnimation.skeleton.scaleY = 0.05;
   });
 
 engine.run();
