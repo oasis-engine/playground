445315bcb5e11a56ef76c45b60dfa0a2bca64c8d
cptbtptpbcptdtptp <njktsmshh@163.com>
2021-04-20 21:31:21 +0800

    feat：[2D] add animated sprite demo

--- demos/sprite/sprite-frameAnimation/index.ts
+++ demos/sprite/sprite-frameAnimation/index.ts
@@ -1,78 +1,78 @@
 import { OrbitControl } from "@oasis-engine/controls";
-import { Entity } from "oasis-engine";
 import { Vector2 } from "oasis-engine";
-import { Rect } from "oasis-engine";
 import { AssetType, Camera, Script, Sprite, SpriteRenderer, SystemInfo, Texture2D, WebGLEngine } from "oasis-engine";
 
-// Create engine object
-const engine = new WebGLEngine("o3-demo");
-engine.canvas.width = window.innerWidth * SystemInfo.devicePixelRatio;
-engine.canvas.height = window.innerHeight * SystemInfo.devicePixelRatio;
+init();
 
-// Create rootEntity
-const scene = engine.sceneManager.activeScene;
-const rootEntity = scene.createRootEntity();
+function init(): void {
+  // Create engine object.
+  const engine = new WebGLEngine("o3-demo");
+  engine.canvas.width = window.innerWidth * SystemInfo.devicePixelRatio;
+  engine.canvas.height = window.innerHeight * SystemInfo.devicePixelRatio;
 
-// Create spriteEntity
-const spriteEntity = new Entity(engine);
-rootEntity.addChild(spriteEntity);
-spriteEntity.transform.setScale(10, 10, 10);
+  // Create rootEntity.
+  const rootEntity = engine.sceneManager.activeScene.createRootEntity();
 
-// Create camera
-const cameraEntity: Entity = rootEntity.createChild("camera_entity");
-cameraEntity.transform.setPosition(0, 0, 50);
-cameraEntity.addComponent(Camera);
-cameraEntity.addComponent(OrbitControl);
+  // Create camera.
+  const cameraEntity = rootEntity.createChild("Camera");
+  cameraEntity.transform.setPosition(0, 0, 5);
+  cameraEntity.addComponent(Camera);
+  cameraEntity.addComponent(OrbitControl);
 
-// Load Texture2D
-engine.resourceManager
-  .load({
-    url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*zcZVTKry5R4AAAAAAAAAAAAAARQnAQ",
-    type: AssetType.Texture2D
-  })
-  .then((texture: Texture2D): void => {
-    // add spriteRenderer
-    spriteEntity.addComponent(SpriteRenderer).sprite = new Sprite(engine, texture);
-    spriteEntity.addComponent(FrameSpriteScrip);
-  });
+  // Load texture and create sprite sheet animation.
+  engine.resourceManager
+    .load<Texture2D>({
+      url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*zcZVTKry5R4AAAAAAAAAAAAAARQnAQ",
+      type: AssetType.Texture2D
+    })
+    .then((texture) => {
+      const spriteEntity = rootEntity.createChild("Sprite");
+      spriteEntity.addComponent(SpriteRenderer).sprite = new Sprite(engine, texture);
+      spriteEntity.addComponent(FrameSpriteScript);
+    });
 
-engine.run();
+  // Run engine.
+  engine.run();
+}
 
-// Script for sprite
-class FrameSpriteScrip extends Script {
-  // FrameAniamtion renderer
-  private _renderer: SpriteRenderer;
-  // Group of region
+/**
+ * Script for sprite sheet animation.
+ */
+class FrameSpriteScript extends Script {
+  /** Offsets of sprite sheet animation. */
   private _regions: Vector2[];
-  // Reciprocal Of SliceWidth
-  private _reciprocalOfSliceWidth: number;
-  // Reciprocal Of SliceHeight
-  private _reciprocalOfSliceHeight: number;
-  // Total frames
-  private _totalFrames: number = 16;
-  // Frame interval time
+  /** Reciprocal Of SliceWidth. */
+  private _reciprocalSliceWidth: number;
+  /** Reciprocal Of SliceHeight. */
+  private _reciprocalSliceHeight: number;
+  /** Total frames. */
+  private _totalFrames: number;
+  /** Frame interval time, the unit of time is ms. */
   private _frameInterval: number = 100;
-  // Now index of frames
-  private _frameIndex: number;
-  private _reuseRect: Rect = new Rect(0, 0, 0, 0);
+
+  private _sprite: Sprite;
+  private _curFrameIndex: number;
   private _cumulativeTime: number = 0;
 
   onAwake(): void {
-    this._renderer = this._entity.getComponent(SpriteRenderer);
-    const row = 4,
-      line = 4,
-      reciprocalOfSliceWidth = 1 / row,
-      reciprocalOfSliceHeight = 1 / line;
-    const regions: Vector2[] = [];
-    for (let j = 0; j < line; j++) {
-      let tempY = j * reciprocalOfSliceHeight;
-      for (let i = 0; i < row; i++) {
-        regions.push(new Vector2(i * reciprocalOfSliceWidth, tempY));
+    // Sprite sheet animation pictures have 4 rows and 4 columns, if you modify the picture, please modify this.
+    const row = 4;
+    const col = 4;
+    const reciprocalSliceWidth = 1 / row;
+    const reciprocalSliceHeight = 1 / col;
+    const regions = new Array<Vector2>();
+    for (let i = 0; i < col; i++) {
+      const y = i * reciprocalSliceHeight;
+      for (let j = 0; j < row; j++) {
+        regions.push(new Vector2(j * reciprocalSliceWidth, y));
       }
     }
+
+    this._sprite = this._entity.getComponent(SpriteRenderer).sprite;
     this._regions = regions;
-    this._reciprocalOfSliceWidth = reciprocalOfSliceWidth;
-    this._reciprocalOfSliceHeight = reciprocalOfSliceHeight;
+    this._reciprocalSliceWidth = reciprocalSliceWidth;
+    this._reciprocalSliceHeight = reciprocalSliceHeight;
+    this._totalFrames = row * col;
     this._setFrameIndex(0);
   }
 
@@ -80,29 +80,24 @@ class FrameSpriteScrip extends Script {
     if (this._totalFrames <= 0) {
       return;
     }
-    const { _frameInterval } = this;
+    const frameInterval = this._frameInterval;
     this._cumulativeTime += deltaTime;
 
-    if (this._cumulativeTime >= _frameInterval) {
-      // need update frameIndex
-      const addTimes = Math.floor(this._cumulativeTime / _frameInterval);
-      this._cumulativeTime -= addTimes * _frameInterval;
-      this._setFrameIndex((this._frameIndex + addTimes) % this._totalFrames);
+    if (this._cumulativeTime >= frameInterval) {
+      // Need update frameIndex.
+      const addFrameCount = Math.floor(this._cumulativeTime / frameInterval);
+      this._cumulativeTime -= addFrameCount * frameInterval;
+      this._setFrameIndex((this._curFrameIndex + addFrameCount) % this._totalFrames);
     }
   }
 
-  // Index of frames
-  private _setFrameIndex(frameIndex: number) {
-    if (this._frameIndex != frameIndex) {
-      console.log(frameIndex);
-      this._frameIndex = frameIndex;
+  private _setFrameIndex(frameIndex: number): void {
+    if (this._curFrameIndex !== frameIndex) {
+      this._curFrameIndex = frameIndex;
       const frameInfo = this._regions[frameIndex];
-      this._renderer.sprite.region = this._reuseRect.setValue(
-        frameInfo.x,
-        frameInfo.y,
-        this._reciprocalOfSliceWidth,
-        this._reciprocalOfSliceHeight
-      );
+      const region = this._sprite.region;
+      region.setValue(frameInfo.x, frameInfo.y, this._reciprocalSliceWidth, this._reciprocalSliceHeight);
+      this._sprite.region = region;
     }
   }
 }
