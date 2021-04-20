f72413de4f04ffbe1231b34890f5856ea919156b
cptbtptpbcptdtptp <njktsmshh@163.com>
2021-04-20 17:36:05 +0800

    feat：[2D] add animated sprite demo

--- demos/sprite/sprite-frameAnimation/index.ts
+++ demos/sprite/sprite-frameAnimation/index.ts
@@ -1,18 +1,8 @@
-import {OrbitControl} from "@oasis-engine/controls";
-import {Entity} from "oasis-engine";
-import {Vector2} from "oasis-engine";
-import {Rect} from "oasis-engine";
-import {
-    AssetType,
-    Camera,
-    Script,
-    Sprite,
-    SpriteRenderer,
-    SystemInfo,
-    Texture2D,
-    Vector3,
-    WebGLEngine
-} from "oasis-engine";
+import { OrbitControl } from "@oasis-engine/controls";
+import { Entity } from "oasis-engine";
+import { Vector2 } from "oasis-engine";
+import { Rect } from "oasis-engine";
+import { AssetType, Camera, Script, Sprite, SpriteRenderer, SystemInfo, Texture2D, WebGLEngine } from "oasis-engine";
 
 // Create engine object
 const engine = new WebGLEngine("o3-demo");
@@ -35,69 +25,83 @@ cameraEntity.addComponent(Camera);
 cameraEntity.addComponent(OrbitControl);
 
 // Load Texture2D
-engine.resourceManager.load({url: 'https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*oth4QoD2ub8AAAAAAAAAAAAAARQnAQ', type: AssetType.Texture2D}).then((texture : Texture2D) : void => { // add spriteRenderer
+engine.resourceManager
+  .load({
+    url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*oth4QoD2ub8AAAAAAAAAAAAAARQnAQ",
+    type: AssetType.Texture2D
+  })
+  .then((texture: Texture2D): void => {
+    // add spriteRenderer
     spriteEntity.addComponent(SpriteRenderer).sprite = new Sprite(engine, texture);
     spriteEntity.addComponent(FrameSpriteScrip);
-});
+  });
 
 engine.run();
 
 // Script for sprite
-class FrameSpriteScrip extends Script { // FrameAniamtion renderer
-    private _renderer : SpriteRenderer;
-    // Group of region
-    private _regionArr : Array < Vector2 >;
-    // Reciprocal Of SliceWidth
-    private _reciprocalOfSliceWidth : number;
-    // Reciprocal Of SliceHeight
-    private _reciprocalOfSliceHeight : number;
-    // Total frames
-    private _totalFrames : number;
-    // Frame interval time
-    private _frameInterval : number;
-    onAwake(): void {
-        this._renderer = this._entity.getComponent(SpriteRenderer);
-        const row: number = 22,
-            line: number = 3,
-            reciprocalOfSliceWidth: number = 1 / row,
-            reciprocalOfSliceHeight: number = 1 / line;
-        const regionArr: Array < Vector2 > = [];
-        for (let j = 0; j < line; j++) {
-            let tempY = j * reciprocalOfSliceHeight;
-            for (let i = 0; i < row; i++) {
-                regionArr.push(new Vector2(i * reciprocalOfSliceWidth, tempY));
-            }
-        }
-        this._regionArr = regionArr;
-        this._reciprocalOfSliceWidth = reciprocalOfSliceWidth;
-        this._reciprocalOfSliceHeight = reciprocalOfSliceHeight;
-        this._totalFrames = 45;
-        this._frameInterval = 100;
-        this.frameIndex = 0;
-    }
+class FrameSpriteScrip extends Script {
+  // FrameAniamtion renderer
+  private _renderer: SpriteRenderer;
+  // Group of region
+  private _regions: Vector2[];
+  // Reciprocal Of SliceWidth
+  private _reciprocalOfSliceWidth: number;
+  // Reciprocal Of SliceHeight
+  private _reciprocalOfSliceHeight: number;
+  // Total frames
+  private _totalFrames: number = 45;
+  // Frame interval time
+  private _frameInterval: number = 100;
+  // Now index of frames
+  private _frameIndex: number;
+  private _reuseRect: Rect = new Rect(0, 0, 0, 0);
+  private _cumulativeTime: number = 0;
 
-    private _cumulativeTime : number = 0;
-    onUpdate(deltaTime : number): void {
-        if (this._totalFrames<= 0) {
-            return;
-        }
-        const {_frameInterval} = this;
-        this._cumulativeTime += deltaTime;
-        if (this._cumulativeTime >= _frameInterval) { // need update frameIndex
-            var addTimes = Math.floor(this._cumulativeTime / _frameInterval);
-            this._cumulativeTime -= addTimes * _frameInterval;
-            this.frameIndex = (this._frameIndex + addTimes) % this._totalFrames;
-        }
+  onAwake(): void {
+    this._renderer = this._entity.getComponent(SpriteRenderer);
+    const row = 22,
+      line = 3,
+      reciprocalOfSliceWidth = 1 / row,
+      reciprocalOfSliceHeight = 1 / line;
+    const regions: Vector2[] = [];
+    for (let j = 0; j < line; j++) {
+      let tempY = j * reciprocalOfSliceHeight;
+      for (let i = 0; i < row; i++) {
+        regions.push(new Vector2(i * reciprocalOfSliceWidth, tempY));
+      }
     }
+    this._regions = regions;
+    this._reciprocalOfSliceWidth = reciprocalOfSliceWidth;
+    this._reciprocalOfSliceHeight = reciprocalOfSliceHeight;
+    this._setFrameIndex(0);
+  }
 
-    private reuseRect : Rect = new Rect(0, 0, 0, 0);
-    private _frameIndex : number;
-    // update sprite‘s region and pivot
-    private set frameIndex(frameIndex : number) {
-        if (this._frameIndex != frameIndex) {
-            this._frameIndex = frameIndex;
-            const frameInfo = this._regionArr[frameIndex];
-            this._renderer.sprite.region = this.reuseRect.setValue(frameInfo.x, frameInfo.y, this._reciprocalOfSliceWidth, this._reciprocalOfSliceHeight);
-        }
+  onUpdate(deltaTime: number): void {
+    if (this._totalFrames <= 0) {
+      return;
     }
+    const { _frameInterval } = this;
+    this._cumulativeTime += deltaTime;
+
+    if (this._cumulativeTime >= _frameInterval) {
+      // need update frameIndex
+      const addTimes = Math.floor(this._cumulativeTime / _frameInterval);
+      this._cumulativeTime -= addTimes * _frameInterval;
+      this._setFrameIndex((this._frameIndex + addTimes) % this._totalFrames);
+    }
+  }
+
+  // Index of frames
+  private _setFrameIndex(frameIndex: number) {
+    if (this._frameIndex != frameIndex) {
+      this._frameIndex = frameIndex;
+      const frameInfo = this._regions[frameIndex];
+      this._renderer.sprite.region = this._reuseRect.setValue(
+        frameInfo.x,
+        frameInfo.y,
+        this._reciprocalOfSliceWidth,
+        this._reciprocalOfSliceHeight
+      );
+    }
+  }
 }
